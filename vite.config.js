51be47a7acaee96ee95e8d81573ed598fffa8bee
untitled <cirno@lol.com>
2024-04-18 21:52:44 +0700

    update docker-compose

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     server: {
         hmr: {
-            host: '127.0.0.1'
+            host: "127.0.0.1",
         },
     },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
+            input: "resources/js/app.js",
+            ssr: "resources/js/ssr.js",
             refresh: true,
         }),
         vue({
