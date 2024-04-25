0f8039f761d99be7de79d880e58b9209e084858f
untitled <cirno@lol.com>
2024-04-26 05:59:03 +0700

    backend: add public path

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,13 +36,13 @@ return [
             'throw' => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app\\public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-        ],
+        //        'public' => [
+        //            'driver' => 'local',
+        //            'root' => storage_path('app\\public'),
+        //            'url' => env('APP_URL').'/storage',
+        //            'visibility' => 'public',
+        //            'throw' => false,
+        //        ],
 
         's3' => [
             'driver' => 's3',
@@ -59,6 +59,11 @@ return [
             'driver' => 'local',
             'root' => public_path('media'),
             'url' => env('APP_URL').'/media',
+        ],
+        'public' => [
+            'driver' => 'local',
+            'root' => public_path(),
+            'url' => env('APP_URL'),
         ],
 
     ],
