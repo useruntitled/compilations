1b63ed2dd489892d6ec126a308f063ecaf532d75
untitled <cirno@lol.com>
2024-04-27 16:40:06 +0700

    Frontend: Fix code style

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,77 +1,79 @@
-import './bootstrap';
-import '../css/app.css';
+import "./bootstrap";
+import "../css/app.css";
 
-import {createApp, createSSRApp, h} from 'vue'
-import { createI18n } from 'vue-i18n'
-import { createInertiaApp,Head,Link } from '@inertiajs/vue3';
-import {ZiggyVue} from 'ziggy-js';
-import VueLazyLoad from 'vue-lazyload';
-import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
+import { createApp, createSSRApp, h } from "vue";
+import { createI18n } from "vue-i18n";
+import { createInertiaApp, Head, Link } from "@inertiajs/vue3";
+import { ZiggyVue } from "ziggy-js";
+import VueLazyLoad from "vue-lazyload";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
-
-
-const appName = import.meta.env.VITE_APP_NAME || 'fcbc';
-
+const appName = import.meta.env.VITE_APP_NAME || "fcbc";
 
 function customRule(choice, choicesLength, orgRule) {
-  if (choice === 0) {
-    return 0;
-  }
-
-  const teen = choice > 10 && choice < 20;
-  if (!teen) {
-    if (choice % 10 === 1) {
-      return 1;
-    } else if (choice % 10 >= 2 && choice % 10 <= 4) {
-      return 2;
+    if (choice === 0) {
+        return 0;
     }
-  }
-  if (choicesLength < 4) {
-    return 2;
-  }
-  return 3;
+
+    const teen = choice > 10 && choice < 20;
+    if (!teen) {
+        if (choice % 10 === 1) {
+            return 1;
+        } else if (choice % 10 >= 2 && choice % 10 <= 4) {
+            return 2;
+        }
+    }
+    if (choicesLength < 4) {
+        return 2;
+    }
+    return 3;
 }
 
 const i18n = createI18n({
-  locale: 'ru',
-  // the custom rules here ...
-  pluralizationRules: {
-    ru: customRule
-  },
-  messages: {
-    ru: {
-      car: '0 машин | {n} машина | {n} машины | {n} машин',
-      banana: 'нет бананов | {n} банан | {n} банана | {n} бананов',
-      answer: '0 ответов | {n} ответ | {n} ответа | {n} ответов',
-      comment: '0 комментариев | {n} комментарий | {n} комментария | {n} комментариев',
-      user: '0 пользователей | {n} пользователь | {n} пользователя | {n} пользователя',
-      film: '0 фильмов | {n} фильм | {n} фильма | {n} фильмов',
-      view: '0 показов | {n} показ | {n} показа | {n} показов',
-      visit: '0 открытий | {n} открытие | {n} открытия | {n} открытий',
-    }
-  }
-})
+    locale: "ru",
+    // the custom rules here ...
+    pluralizationRules: {
+        ru: customRule,
+    },
+    messages: {
+        ru: {
+            car: "0 машин | {n} машина | {n} машины | {n} машин",
+            banana: "нет бананов | {n} банан | {n} банана | {n} бананов",
+            answer: "0 ответов | {n} ответ | {n} ответа | {n} ответов",
+            comment:
+                "0 комментариев | {n} комментарий | {n} комментария | {n} комментариев",
+            user: "0 пользователей | {n} пользователь | {n} пользователя | {n} пользователя",
+            film: "0 фильмов | {n} фильм | {n} фильма | {n} фильмов",
+            view: "0 показов | {n} показ | {n} показа | {n} показов",
+            visit: "0 открытий | {n} открытие | {n} открытия | {n} открытий",
+        },
+    },
+});
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob("./Pages/**/*.vue"),
+        ),
     setup({ el, App, props, plugin }) {
-        return createApp({render: () => h(App, props)})
+        return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
             .use(VueLazyLoad, {
                 attempt: 3,
-                loading: '/preloader.png',
+                loading: "/preloader.png",
                 // the default is ['scroll', 'wheel', 'mousewheel', 'resize', 'animationend', 'transitionend']
             })
             .use(i18n)
-            .component('Head',Head)
-            .component('Link',Link)
+            .component("Head", Head)
+            .component("Link", Link)
 
-            .mount(el)
+            .mount(el);
     },
 
     progress: {
-        color: '#d4620b',
+        color: "#d4620b",
     },
 });
--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,17 +1,16 @@
-import {createInertiaApp} from '@inertiajs/vue3'
-import createServer from '@inertiajs/vue3/server'
-import {renderToString} from '@vue/server-renderer'
-import {createSSRApp, h} from 'vue'
-import {ZiggyVue} from 'ziggy-js';
-import {Head} from '@inertiajs/vue3'
-import {Link} from '@inertiajs/vue3'
-import '../css/app.css';
-import {createI18n} from "vue-i18n";
-import VueLazyLoad from 'vue-lazyload';
-import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
+import { createInertiaApp } from "@inertiajs/vue3";
+import createServer from "@inertiajs/vue3/server";
+import { renderToString } from "@vue/server-renderer";
+import { createSSRApp, h } from "vue";
+import { ZiggyVue } from "ziggy-js";
+import { Head } from "@inertiajs/vue3";
+import { Link } from "@inertiajs/vue3";
+import "../css/app.css";
+import { createI18n } from "vue-i18n";
+import VueLazyLoad from "vue-lazyload";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
-const appName = import.meta.env.VITE_APP_NAME || 'fcbc';
-
+const appName = import.meta.env.VITE_APP_NAME || "fcbc";
 
 function customRule(choice, choicesLength, orgRule) {
     if (choice === 0) {
@@ -33,41 +32,45 @@ function customRule(choice, choicesLength, orgRule) {
 }
 
 const i18n = createI18n({
-    locale: 'ru',
+    locale: "ru",
     // the custom rules here ...
     pluralizationRules: {
-        ru: customRule
+        ru: customRule,
     },
     messages: {
         ru: {
-            car: '0 машин | {n} машина | {n} машины | {n} машин',
-            banana: 'нет бананов | {n} банан | {n} банана | {n} бананов',
-            answer: '0 ответов | {n} ответ | {n} ответа | {n} ответов',
-            comment: '0 комментариев | {n} комментарий | {n} комментария | {n} комментариев',
-            user: '0 пользователей | {n} пользователь | {n} пользователя | {n} пользователя',
-            film: '0 фильмов | {n} фильм | {n} фильма | {n} фильмов',
-            view: '0 показов | {n} показ | {n} показа | {n} показов',
-            visit: '0 открытий | {n} открытие | {n} открытия | {n} открытий',
-        }
-    }
-})
+            car: "0 машин | {n} машина | {n} машины | {n} машин",
+            banana: "нет бананов | {n} банан | {n} банана | {n} бананов",
+            answer: "0 ответов | {n} ответ | {n} ответа | {n} ответов",
+            comment:
+                "0 комментариев | {n} комментарий | {n} комментария | {n} комментариев",
+            user: "0 пользователей | {n} пользователь | {n} пользователя | {n} пользователя",
+            film: "0 фильмов | {n} фильм | {n} фильма | {n} фильмов",
+            view: "0 показов | {n} показ | {n} показа | {n} показов",
+            visit: "0 открытий | {n} открытие | {n} открытия | {n} открытий",
+        },
+    },
+});
 
-
-createServer(page =>
+createServer((page) =>
     createInertiaApp({
         page,
         render: renderToString,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-        setup({App, props, plugin}) {
-            return createSSRApp({render: () => h(App, props)})
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.vue`,
+                import.meta.glob("./Pages/**/*.vue"),
+            ),
+        setup({ App, props, plugin }) {
+            return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
                 .use(ZiggyVue, {
                     ...page.props.ziggy,
                     location: new URL(page.props.ziggy.location),
                 })
                 .use(i18n)
-                .component('Head', Head)
-                .component('Link', Link)
+                .component("Head", Head)
+                .component("Link", Link);
         },
     }),
-)
+);
