66c64a52ce85ada8bc21ae3c6f3a12be14fa6994
untitled <cirno@lol.com>
2024-04-27 16:34:03 +0700

    Fix code style

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,6 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-
         ],
 
         'api' => [
